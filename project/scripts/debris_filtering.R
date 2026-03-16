2f3e4b5574df4a7d86b9256f2dabfc8007a07709
Jacob Pollard <jacobpollard2915@gmail.com>
2026-03-16 14:09:19 +0000

    Load both datasets

--- project/scripts/debris_filtering.R
+++ project/scripts/debris_filtering.R
@@ -1,10 +1,36 @@
 # ============================================================
 # Livecyte Data Analysis
-# - Debris filtering
+# - Data assembling and debris filtering
 # ============================================================
 #
-# Load libraries ---------------------------------------------
+# 1. Load libraries ---------------------------------------------
 #
 library(tidyverse)
 #
-# 
+# 2. Load data --------------------------------------------------
+#
+# 2a. Load the livecyte automatic dataset
+#
+livecyte <- read_tsv("project/data/movement_morphology/livecyte_data.tsv",
+                     col_types = cols(
+                       clone = col_factor(),
+                       replicate = col_factor(),
+                       tracking.id = col_factor(),
+                       lineage.id = col_factor()
+                     )) # Ensure that categorical variables are read as factors
+#
+# 2b. Load the manual dataset
+#
+manual <- read_tsv("project/data/movement_morphology/manual_data.tsv",
+                   col_types = cols(
+                     track.present.at.start.or.end = col_factor(),
+                     track.never.divides = col_factor(),
+                     cell.line = col_factor()
+                   ))
+#
+# 3. Filter out debris from livecyte dataset --------------------
+#
+# 3a. Initially, we can eliminate debris from colony A, replicate 1 and colony B, replicate 2 by using the manual dataset.
+#
+#
+
