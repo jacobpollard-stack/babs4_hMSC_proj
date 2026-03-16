57dc9d7fd3cc92fb2be0f9da668352c453858da3
Jacob Pollard <jacobpollard2915@gmail.com>
2026-03-16 15:24:48 +0000

    Calculate discrepancy between displacement values

--- project/scripts/debris_filtering.R
+++ project/scripts/debris_filtering.R
@@ -1,6 +1,6 @@
 # ============================================================
 # Livecyte Data Analysis
-# - Data assembling and debris filtering
+# - Data assembling, tidying, and debris filtering
 # ============================================================
 #
 # 1. Load libraries ---------------------------------------------
@@ -29,8 +29,44 @@ manual <- read_tsv("project/data/movement_morphology/manual_data.tsv",
                    ))
 #
 # 3. Filter out debris from livecyte dataset --------------------
+
+# 3a. Collapse all the data into one nice table
 #
-# 3a. Initially, we can eliminate debris from colony A, replicate 1 and colony B, replicate 2 by using the manual dataset.
+livecyte_pretty <- livecyte |> 
+  arrange(clone, replicate, lineage.id, tracking.id, frame) |>
+  group_by(clone, replicate, lineage.id, tracking.id) |>
+  summarise(
+    n_frames = n(),
+    start_frame = first(frame),
+    total_path_length = last(track.length),
+    final_displacement = last(displacement), # As displacement is cumulative
+    calculated_displacement = sqrt((last(position.x) - first(position.x))^2 +
+                                     (last(position.y) - first(position.y))^2), # Calculate displacement from start to end position to double check that the final displacement is correctly calculated by Livecyte
+    .groups = "drop") |> # Drop groups after summarising
+  mutate(
+    lineage_rootcell = start_frame == min(start_frame),
+    left_censored = ! lineage_rootcell & start_frame > 1) # Flags objects that appear mid-experiment but are not daughter or root cells ie. debris
 #
+# 3b. Check whether the final displacement calculated from the start and end positions matches the cumulative displacement provided by Livecyte. Also check whether lineage.id and tracking.id diverge at any point
 #
+livecyte_pretty <- livecyte_pretty |>
+  mutate(displacement_equal = final_displacement == calculated_displacement)
+livecyte_pretty <- livecyte_pretty |>
+  mutate(lineage_equal = lineage.id == tracking.id)
+# We can see that there are some discrepancies between the final displacement and the calculated displacement, which suggests that there may be some errors in the tracking.
+# Therefore we will perform a Wilcoxon test to see if there is a significant difference between the final displacement and the calculated displacement. If there is a significant difference, we will need to investigate further to see if there are any systematic errors in the tracking.
+wilcox.test(livecyte_pretty$final_displacement, livecyte_pretty$calculated_displacement, paired = TRUE)
+#
+livecyte_pretty |>
+  filter(!lineage_equal)
+# lineage.id and tracking.id are equal for all objects, which suggests that mitosis did not occur during the experiment. However, we can see cell division happening in the video we collected. Thus, the Livecyte data must be wrong or missing some information.
+#
+# 3c. Collapsing the manual dataset
+#
+
+
+
+# 3d. Initially, we can eliminate debris from colony A, replicate 1 and colony B, replicate 2 by using the manual dataset that only includes cells and no debris.
+# 
+
 
