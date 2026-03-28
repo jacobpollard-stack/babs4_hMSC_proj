ac5b64293c8970f27f49b76be99b1c40a1aa251a
Jacob Pollard <jacobpollard2915@gmail.com>
2026-03-28 13:37:30 +0000

    Give up

--- project/scripts/debris_filtering_draft2.R
+++ project/scripts/debris_filtering_draft2.R
@@ -98,38 +98,42 @@ mean.speed_manual <- ggplot(manual, aes(x = mean.speed)) +
 ggplot() +
   geom_histogram(data = manual, aes(x = euclidean.distance), fill = "blue", alpha = 0.5, binwidth = 30) +
   geom_histogram(data = livecyte_filtered, aes(x = final_displacement), fill = "red", alpha = 0.5, binwidth = 30) +
-  facet_wrap(~ cell.line) +
-  labs(title = "Overlay of Euclidean Distance (Manual) and Final Displacement (Livecyte)", x = "Distance (µm)", y = "Count") +
-  theme_minimal()
+  facet_wrap(~ cell.line)
 #
 # track.length and total_path_length
 ggplot() +
   geom_histogram(data = manual, aes(x = track.length), fill = "blue", alpha = 0.5, binwidth = 30) +
   geom_histogram(data = livecyte_filtered, aes(x = total_path_length), fill = "red", alpha = 0.5, binwidth = 30) +
-  facet_wrap(~ cell.line) +
-  labs(title = "Overlay of Track Length (Manual) and Total Path Length (Livecyte)", x = "Length (µm)", y = "Count") +
-  theme_minimal()
+  facet_wrap(~ cell.line)
 
 # mean.speed
 ggplot() +
   geom_histogram(data = manual, aes(x = mean.speed), fill = "blue", alpha = 0.5, binwidth = 0.01) +
   geom_histogram(data = livecyte_filtered, aes(x = mean.speed), fill = "red", alpha = 0.5, binwidth = 0.01) +
-  facet_wrap(~ cell.line) +
-  labs(title = "Overlay of Mean Speed (Manual) and Mean Speed (Livecyte)", x = "Mean Speed (µm/min)", y = "Count") +
-  theme_minimal()
+  facet_wrap(~ cell.line)
 
 # Clone A appears more likely for the automated Livecyte tracking to lose cells, leading to a dramatically different track length + mean.speed distributions for A1 than B2
 
 # We see clearly that the mean speed and track length distributions are very different between the manual and livecyte datasets, suggesting that the mean speed values in the livecyte dataset are not reliable and should not be used for filtering out debris. The euclidean distance distribution is more similar between the manual and livecyte datasets, suggesting that we can use the same thresholds for filtering out debris from the livecyte dataset based on this parameter. However, we should be cautious when interpreting the results, as there may still be some differences between the two datasets that could affect the analysis.
 
-# 3bv. Perform a KS and  Wilcoxon test to compare the distributions of euclidean distance in manual  and final displacement in livecyte_filtered.
-ks_test <- ks.test(manual$euclidean.distance, livecyte_filtered$final_displacement)
-wilcox_test <- wilcox.test(manual$euclidean.distance, livecyte_filtered$final_displacement)
-
-# KS: p-value = 0.6319
-# Wilcoxon: p-value = 0.9426
+# 3bv. Perform a KS and  Wilcoxon test to compare the distributions of euclidean distance in manual and final displacement in livecyte_filtered.
+ks_test_euc <- ks.test(manual$euclidean.distance, livecyte_filtered$final_displacement)
+wilcox_test_euc <- wilcox.test(manual$euclidean.distance, livecyte_filtered$final_displacement)
+print(ks_test_euc)
+print(wilcox_test_euc)
+# KS: D = 0.074627, p-value = 0.6319
+# Wilcoxon: W = 20184, p-value = 0.9426
 # The KS and Wilcoxon tests suggest that there is no significant difference between the distributions of euclidean distance in the manual dataset and final displacement in the livecyte_filtered dataset, meaning that we can use this parameter for filtering debris from the livecyte dataset.
 
+# 3bvi. Perform a KS and Wilcoxon test to compare the distributions of track.length in manual and total_path_length in livecyte_filtered.
+ks_test_track <- ks.test(manual$track.length, livecyte_filtered$total_path_length)
+wilcox_test_track <- wilcox.test(manual$track.length, livecyte_filtered$total_path_length)
+print(ks_test_track)
+print(wilcox_test_track)
+# KS: D = 0.30341, p-value = 1.928e-08
+# Wilcoxon: W = 22746, p-value = 0.02262
+# The KS and Wilcoxon tests suggest that there is a significant difference between the distributions of track.length in the manual dataset and total_path_length in the livecyte_filtered dataset, meaning that we should not use this parameter for filtering debris from the livecyte dataset.
+#
 # 4. Test whether the distributions of final displacement are roughly homogenous across the different replicates in a single clone.
 # We will use an Anderson-Darling k-sample and Kruskal-Wallis tests.
 # 4a. Split livecyte_pretty into two datasets, one for each clone.
@@ -137,7 +141,7 @@ livecyte_A <- livecyte_pretty |>
   filter(clone == "cloneA")
 livecyte_B <- livecyte_pretty |>
   filter(clone == "cloneB")
-# 4bi. Perform Anderson-Darling k-sample test for each new dataset.
+# 4bi. Perform Anderson-Darling (AD) k-sample test for each new dataset.
 # Clone A
 ad_test_A <- do.call(ad.test, split(livecyte_A$final_displacement, livecyte_A$replicate))
 # Clone B
@@ -158,14 +162,39 @@ pairwise.wilcox.test(livecyte_B$final_displacement,
 ggplot(livecyte_B, aes(x = final_displacement, colour = replicate)) +
   stat_ecdf()
 # The plot shows that replicate 3 is significantly higher in final displacement than replicates 1 and 2, which are more similar to each other. This suggests that replicate 3 may contain more debris or tracking errors, which could be affecting the distribution of final displacement in clone B.
-
-
-
-# 4c. Perform Kruskal-Wallis test for each new dataset.
-# Clone A
-kruskal_test_A <- kruskal.test(final_displacement ~ replicate, data = livecyte_A)
-# Clone B
-kruskal_test_B <- kruskal.test(final_displacement ~ replicate, data = livecyte_B)
-# Print results
-print(kruskal_test_A)
-print(kruskal_test_B)
+#
+# 4c. Discover whether replicate 3 is different because of a higher proportion of debris, or because the cells in that replicate are simply more motile; ie. is the difference real?
+# 4ci. Create a dataset containing the objects in livecyte_pretty that were not matched to a real cell
+livecyte_debris <- livecyte_pretty |>
+  anti_join(tid_mapping, by = c("clone", "replicate", "tracking.id")) |>
+  filter(clone == "cloneB")
+# 4cii. Perform statistical tests to compare the distribution of final displacement in the debris dataset to the distribution of final displacement in the matched dataset (livecyte_B).
+# AD test
+ad_debris_B <- do.call(ad.test, 
+                       split(livecyte_debris$final_displacement, 
+                             livecyte_debris$replicate))
+print(ad_debris_B)
+#
+#  AD   T.AD  asympt. P-value
+# version 1: 28.888 24.982       2.5318e-15
+# version 2: 28.900 24.987       2.2875e-15
+#
+# Wilcoxon test
+wilcoxon_debris_B <- pairwise.wilcox.test(livecyte_debris$final_displacement,
+                     livecyte_debris$replicate,
+                     p.adjust.method = "BH")
+print(wilcoxon_debris_B)
+#
+#   1       2      
+# 2 0.079   -      
+# 3 4.3e-11 3.8e-10
+#
+# The tests suggest that the distribution of debris in clone B replicate 3 is significantly different from the distribution of debris in replicates 1 and 2, which are more similar to each other. This suggests that replicate 3 may contain more debris or tracking errors, which could be affecting the distribution of final displacement in clone B.
+# Thus, we will filter out replicate 3 from clone B in our livecyte dataset, as it appears to contain a higher proportion of debris or tracking errors, which could be affecting the analysis of cell motility in this clone.
+#
+# 4ciii.
+livecyte_pretty <- livecyte_pretty |>
+  filter(!(clone == "cloneB" & replicate == "3"))
+#
+#
+# This is all very complicated and creating more problems than it's solving. Cells and debris overlap significantly across all parameters, making accurate filtering borderline impossible. I'm sure a better statistician could do it, but not I.
