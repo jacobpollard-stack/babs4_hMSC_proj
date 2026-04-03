b1655a0436db6e577d3b63691404af39f8001f6c
Jacob Pollard <jacobpollard2915@gmail.com>
2026-04-03 11:15:32 +0100

    Plot 3D graph of PC1, 2, and 3

--- project/scripts/statistics/principle_component_analysis.R
+++ project/scripts/statistics/principle_component_analysis.R
@@ -31,12 +31,15 @@ pca_result <- prcomp(numeric_metrics, scale. = TRUE)
 pca_df <- as.data.frame(pca_result$x) |> 
   bind_cols(metrics |> select(-where(is.numeric)))
 #
-# 3c. Plot PCA results in 2d
+# 3c. Plot PCA results in 2d with ellipses
 #
 ggplot(pca_df, aes(x = PC1, y = PC2, colour = clone)) +
   geom_point(size = 3) +
   labs(x = 'Morphology',
-       y = 'Movement')
+       y = 'Movement') +
+  theme_minimal() +
+  stat_ellipse(level = 0.95, aes(fill = clone), alpha = 0, geom = "polygon", show.legend = FALSE) +
+  theme(legend.position = "right")
 #
 # We see that dry.mass and volume are perfectly correlated. This is expected as dry.mass is a function of volume, calculated by the microscope.
 #
@@ -46,3 +49,5 @@ plot_ly(pca_df, x = ~PC1, y = ~PC2, z = ~PC3, color = ~clone, type = 'scatter3d'
   layout(scene = list(xaxis = list(title = 'Morphology'),
                       yaxis = list(title = 'Movement'),
                       zaxis = list(title = 'Persistence')))
+#
+# The 3D plot shows that the clones form two seperate clouds in the PCA space, with some overlap. This indicates that, when considering morphology, movement, and 'persistence' (how long it stays on screen, and its mean speed), the two clones are largely distinct. This suggests that the clones have different phenotypic profiles, which could be due to different osteogenic potential.
