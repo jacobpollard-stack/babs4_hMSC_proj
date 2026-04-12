30739564fa8714a8749fa41ed1f211edaba1a021
Jacob Pollard <jacobpollard2915@gmail.com>
2026-04-12 18:11:08 +0100

    Formed and exported osteogenesis plot

--- project/scripts/osteogenesis_tests.R
+++ project/scripts/osteogenesis_tests.R
@@ -7,6 +7,7 @@
 library(tidyverse)
 library(rcompanion)
 library(dunn.test)
+library(ggh4x)
 #
 # 2. Load data -------------------------------------------------------------
 #
@@ -20,107 +21,93 @@ osteo_long <- osteo |>
 #
 # 4. Statistical tests -----------------------------------------------------
 #
-# 4a. Mann-Whitney U Test
+# 4a. Clone A vs clone B at day 0
 #
-# We're using a Mann-Whitney test as we're testing the statistical significance of the differences in absorption between days, and clones. The sample 
+# 4ai. Subset data for day 0 to quantify the effect of clone at baseline
 #
-# 4ai. Subset data for day 8 and 8osteo to quantify the effect of clone and osteogenic medium
-#
-osteo_day8 <- osteo_long |>
-  filter(day %in% c("8", "8osteo"))
-#
-# 4aii. Perform test
-#
-srh_day8 <- scheirerRayHare(absorption ~ clone + day, data = osteo_day8)
-#
-# 4b. Post-hoc Dunn test
-#
-# 4bi. Create a combined factor for clone and day
-#
-osteo_day8 <- osteo_day8 |>
-  mutate(group = interaction(clone, day))
-
-dunn_day8 <- dunn.test(osteo_day8$absorption, osteo_day8$group, method = "bh")
-#
-# Col Mean-│
-# Row Mean │        A.8   A.8osteo        B.8
-# ─────────┼─────────────────────────────────
-# A.8osteo │  -2.038098
-#          │     0.0415*
-#          │
-# B.8      │   1.019049   3.057147
-#          │     0.1541     0.0067**
-#          │
-# B.8osteo │  -1.019049   1.019049  -2.038098
-#               0.1849     0.2311     0.0623
-#
-# The Dunn test results show a significant difference between A.8 and A.8osteo (p = 0.0415) and between A.8osteo and B.8 (p = 0.0067); this is not important. No significant difference exists between the other groups. This suggests that the osteogenic medium has a significant effect on absorption for clone A, but not for clone B (0.0623). In addition, at alpha=0.05, clone A and B did not exhibit significant differences in absorption, regardless of the presence of osteogenic medium. However, the absolute difference in absorption between B.8 and B.8osteo is quite large (~0.282 vs ~0.453), and the p-value is close to the significance threshold, suggesting that there may be a trend towards significance that could be worth further investigation with a larger number of technical replicates.
-#
-# 5. Clone A vs clone B at day 0
-#
-# 5a. Subset data for day 0 to quantify the effect of clone at baseline
-#
-osteo_day0 <- osteo_long |>
+osteo_long_day0 <- osteo_long |>
   filter(day == "0")
 #
-# 5b. Perform wilcoxon rank-sum test
+# 4aii. Perform wilcoxon rank-sum test
 #
-wilcox_AB_day0 <- wilcox.test(absorption ~ clone, data = osteo_day0)
+wilcox_AvB_day0 <- wilcox.test(absorption ~ clone, data = osteo_long_day0, exact = FALSE) # as some data points are identical
 #
-# The Wilcoxon rank-sum test results show no significant difference in absorption between clone A and clone B at day 0 (p = 0.0765225), suggesting that the two clones have similar baseline absorption levels before the introduction of osteogenic medium. 
+# The Wilcoxon rank-sum test results show no significant difference in absorption between clone A and clone B at day 0 (p = 0.0765225), suggesting that the two clones have similar baseline absorption levels before the introduction of osteogenic medium. However it seems as this is due to low sample size (n = 3). |Δ| ≈ 0.05, which is quite large for this data. Thus it seems that the baseline absorption is different between the clones, but the statistical power to detect this difference is very low.
 #
-# 6. Day 8 vs day 0 within each clone
+# 4b. Day 8 vs day 0 within each clone
 #
-# 6a. Clone A
+# 4bi. Clone A
 #
-wilcox_A_day <- wilcox.test(
+wilcox_A_day8v0 <- wilcox.test(
   osteo_long$absorption[osteo_long$clone == "A" & osteo_long$day == "0"],
-  osteo_long$absorption[osteo_long$clone == "A" & osteo_long$day == "8"]
-)
+  osteo_long$absorption[osteo_long$clone == "A" & osteo_long$day == "8"], exact = FALSE)
 #
-# 6b. Clone B
+# 4bii. Clone B
 #
-wilcox_B_day <- wilcox.test(
+wilcox_B_day8v0 <- wilcox.test(
   osteo_long$absorption[osteo_long$clone == "B" & osteo_long$day == "0"],
-  osteo_long$absorption[osteo_long$clone == "B" & osteo_long$day == "8"]
-)
+  osteo_long$absorption[osteo_long$clone == "B" & osteo_long$day == "8"], exact = FALSE)
 #
-# The Wilcoxon rank-sum test results show that the differences in absorption between day 0 and day 8 for both clones are not statistically significant (clone A: p = 0.0765225; clone B: p = 0.1). This suggests that ALP  activity did not change significantly over time for either clone in the absence of osteogenic medium, indicating that the observed differences at day 8 may be primarily driven by the presence of osteogenic medium rather than time alone.
+# The Wilcoxon rank-sum test results show that the differences in absorption between day 0 and day 8 for both clones are not statistically significant (clone A: p = 0.0765225; clone B: p = 0.1). However this is also likely due to small sample size, as the data does show an increase in absorption (Δ(A) ≈ 0.17, Δ(B) ≈ 0.2).
 #
-# 7. Although the Wilcoxon rank-sum test results show no significant differences in absorption between day 0 and day 8 for either clone, it may still be informative to compare the change in absorption over time between the two clones to see if there are any trends that could be worth further investigation with a larger number of technical replicates.
+# 4c. Wilcoxon test on the change in absorption between day 8 and day 0 between clones
 #
-# 7a. Wilcoxon test on the change in absorption between day 8 and day between clones
+# 4ci. Calculate the change in absorption between day 8 and day 0 for each clone
 #
-# 7ai. Calculate the change in absorption between day 8 and day 0 for each clone
-#
-day_change <- osteo_long %>%
-  group_by(clone, replicate) %>%
+day_change <- osteo_long |> 
+  group_by(clone, replicate) |> 
   summarise(change = absorption[day == "8"] - absorption[day == "0"])
 #
-# 7aii. Perform Wilcoxon test on the change in absorption
+# 4cii. Perform Wilcoxon test on the change in absorption
 #
-wilcox_day_change <- wilcox.test(change ~ clone, data = day_change)
+wilcox_AvB_0to8 <- wilcox.test(change ~ clone, data = day_change)
 #
-# The Wilcoxon rank-sum test results show no significant difference in the change in absorption between clone A and clone B (p = 0.1), suggesting that the overall change in ALP activity from day 0 to day 8 is not significantly different between the two clones.
+# The Wilcoxon rank-sum test results show no significant difference in the change in absorption between clone A and clone B (p = 0.1), suggesting that the overall change in ALP activity from day 0 to day 8 is not significantly different between the two clones. Δ for B looks slightly higher than Δ for A, but we will assume statistical non-significance for ease of the next step.
 #
-# 7b. Test change in absorption between day 8 with osteogenic medium and day 8 control between clones
+# 4d. Test change in absorption between day 8 with osteogenic medium and day 8 control between clones
 #
-# 7bi. Calculate the change in absorption between day 8 with osteogenic medium and day 8 control for each clone
+# 4di. Calculate the change in absorption between day 8 with osteogenic medium and day 8 control for each clone
 #
-osteo_change <- osteo_long %>%
-  group_by(clone, replicate) %>%
+osteo_change <- osteo_long |>
+  group_by(clone, replicate) |> 
   summarise(change = absorption[day == "8osteo"] - absorption[day == "8"])
 #
-# 7bii. Perform Wilcoxon test on the change in absorption between clone A and clone B
+# 4dii. Perform Wilcoxon test on the change in absorption between clone A and clone B
 #
 wilcox_osteo_change <- wilcox.test(change ~ clone, data = osteo_change)
 #
-# The Wilcoxon rank-sun test shows no significant difference between the change in the two clone' absorption values between day 8 with osteogenic medium and day 8 control (p = 0.1), suggesting that the effect of osteogenic medium on ALP activity is not significantly different between the two clones.
+# The Wilcoxon rank-sun test shows no significant difference between the change in the two clone' absorption values between day 8 with osteogenic medium and day 8 control (p = 0.1). However, looking at the data, the difference between the clones is about 0.196, which is quite large.
 #
-# 8. Summary of results
+# 5. Plot the osteogenesis data
 #
-# - The Scheirer–Ray–Hare test revealed a significant effect of osteogenic medium on absorption for clone A, but not for clone B.
-# - The Dunn post-hoc test showed significant differences between A.8 and A.8osteo, and between A.8osteo and B.8, but not between the other groups. However, the difference between B.8 and B.8osteo was close to significance, suggesting a potential trend that may warrant further investigation.
-# - The Wilcoxon rank-sum tests showed no significant differences in absorption between clone A and clone B at day 0, and no significant changes in absorption over time for either clone in the absence of osteogenic medium. Additionally, there were no significant differences in the change in absorption between day 8 and day 0, or between day 8 with osteogenic medium and day 8 control, between the two clones.
+osteo_plot <- ggplot(osteo_long, aes(x = day, y = absorption, group = replicate)) +
+  geom_line(aes(colour = clone), alpha = 0.4, linewidth = 0.7) +
+  geom_point(aes(colour = clone), size = 2.5) +
+  facet_wrap(~ clone) +
+  scale_colour_manual(values = c(
+    "A" = "#f7766f",
+    "B" = "#0dc1c5"),
+    guide = "none") +
+  labs(
+    x = "Day",
+    y = "Absorption") +
+  theme(legend.position = "none",
+    strip.text = element_text(face = "bold")) +
+  scale_x_discrete(labels = c(
+    "0" = "0",
+    "8" = "8",
+    "8osteo" = "8 in osteogenic\nmedium")) +
+  scale_y_continuous(n.breaks = 10) +
+  facet_wrap2(~ clone, strip = strip_themed(
+    background_x = list(
+      element_rect(fill = "#f7766f"),
+      element_rect(fill = "#0dc1c5")),
+    text_x = list(
+      element_text(colour = "black", face = "bold"),
+      element_text(colour = "black", face = "bold")))) +
+  theme_bw()
 #
-# Overall, the statistical power of only having 3 technical replicates means that the results should be interpreted with caution, and further investigation with a larger number of replicates is necessary to draw more definitive conclusions about the effects of osteogenic medium and mesenchymal clone on ALP activity.
+# 6. Save plot
+#
+ggsave("project/figures/osteogenesis/osteo_plot.png", osteo_plot)
+
